5db0c1a06945e45ecba8365540014420f3404f3b
JoshuaRosado <joshua06rosado@gmail.com>
2024-09-04 07:46:43 -0500

    Initial Commit

---
+++ BigKidLearning/BigKidLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigKidLearningApp.swift
+//  BigKidLearning
+//
+//  Created by Joshua Rosado Olivencia on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BigKidLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
